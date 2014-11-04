81f2da058ee30048c3826cbcad1bc0a512dce828
mhsaleem <2018657s@student.gla.ac.uk>
2014-11-04 18:04:30 +0000

    Updated return value of uncertainty
    uncertainty now returns whole number instead of decimal double.

--- NaiveRecogniser/src/Counter.java
+++ NaiveRecogniser/src/Counter.java
@@ -1,4 +1,6 @@
 
+import java.text.DecimalFormat;
+
 public class Counter {
 	private int count;
 	private double uncertainty;
@@ -9,21 +11,31 @@ public class Counter {
 	
 	public void reset(){
 		count = 0;
-		error = 0;
+		uncertainty = 0;
 	}
 	
 	public void increment(double uncertainty){
-	if(uncertainty > 0.5){
-		count+=1;
-	}
-		this.uncertainty += (1 - uncertainty)/2; //Updates uncertainty value 
+		if(!(uncertainty > 1 || uncertainty < 0)){
+			if(uncertainty > 0.5){
+				count+=1;
+			}
+				this.uncertainty += (1 - uncertainty)/2; //Updates uncertainty value 
+				DecimalFormat numberFormat = new DecimalFormat("#.00");//used to display uncertainty to two decimal places
+				this.uncertainty = Double.valueOf(numberFormat.format(this.uncertainty)); //updates uncertainty to two decimal places
+				
+		}
+		else{
+			System.out.println("You have entered an invalid uncertainty");
+		}
 	}
 	
-	public int getUncertainty(){
-		return uncertainty; //remember you need to return an integer representing the uncertainty not the precentage
+	
+	public double getUncertainty(){
+		return Math.round(uncertainty*100); //multiply uncertainty by 100 to get whole number value
 	}
 	
 	public int getCount(){
 		return count;
 	}
 }
+
