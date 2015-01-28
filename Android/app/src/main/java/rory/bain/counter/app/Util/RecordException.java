cfb0583f616693c9be62b8e82b8c90016805819b
rorybain <2026138b@student.gla.ac.uk>
2015-01-28 15:28:54 +0000

    Adding history stuff to android

---
+++ Android/app/src/main/java/rory/bain/counter/app/Util/RecordException.java
@@ -0,0 +1,8 @@
+package rory.bain.counter.app.Util;
+public class RecordException extends Exception {
+	public RecordException(){}
+	
+	public RecordException(String message){
+		super(message);
+	}
+}
