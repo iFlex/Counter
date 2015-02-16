de53ec57f1d99b3350fcbb68af14be3f9132878f
unknown <2165422a@student.gla.ac.uk>
2015-02-16 19:33:10 +0000

    Fixed bug in mg recognizer

--- Desktop/src/engine/Processing/algorithms/mgRecogniser.java
+++ Desktop/src/engine/Processing/algorithms/mgRecogniser.java
@@ -23,6 +23,7 @@ public class mgRecogniser extends Recogniser {
 	Wave model,frag;
 	FingerprintSimilarity s;
 	Data toprocess;
+	int debugsaveno;
 	
 	
 	public synchronized void setModel(String name){
@@ -34,14 +35,14 @@ public class mgRecogniser extends Recogniser {
 		System.out.println("MusicG recogniser");
 		counter = c;
 		this.toprocess = new Data();
-		//c.enableIncremental();
+		this.debugsaveno = 0;
 	}
 	
 	@Override
 	public void process(Data data) {
 		this.toprocess.extend(data);
 		
-		if(this.toprocess.getLength() < this.model.getNormalizedAmplitudes().length)
+		if( (this.toprocess.getLength() < 2*this.model.getNormalizedAmplitudes().length) && (data.getLength() != 0) )
 		{
 			return;
 		}
@@ -49,7 +50,7 @@ public class mgRecogniser extends Recogniser {
 		WavFile copy;
 		try
 		{
-			copy = WavFile.newWavFile(new File("./mgrecprocess.wav"), 1, this.toprocess.getLength(), 16, 44100);
+			copy = WavFile.newWavFile(new File("./mgrecprocess"+this.debugsaveno+".wav"), 1, this.toprocess.getLength(), 16, 44100);
 		} catch(Exception e)
 		{
 			e.printStackTrace();
@@ -67,9 +68,22 @@ public class mgRecogniser extends Recogniser {
 			return;
 		}
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		frag = new Wave("./mgrecprocess.wav");
+		frag = new Wave("./mgrecprocess"+this.debugsaveno+".wav");
+		
+		this.debugsaveno++;
+		
 		s = model.getFingerprintSimilarity(frag);
 		System.out.println("Similarity:"+s.getSimilarity());
 		counter.increment(s.getSimilarity());
+		int offset = this.toprocess.getLength()*4/5;
+		int size = this.toprocess.getLength()-offset;
+		double[] newdata = new double[size];
+		double[] olddata = this.toprocess.get();
+		for(int i = 0; i<size; i++)
+		{
+			newdata[i] = olddata[i+offset];
+		}
+		this.toprocess = new Data();
+		this.toprocess.set(newdata);
 	}
 }
