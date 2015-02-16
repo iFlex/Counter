a30b23559b1b18da3f215bc6a332152dae8fdf9c
unknown <2165422a@student.gla.ac.uk>
2015-02-16 18:27:03 +0000

    Made changes to the recognizer so that it creates a wave file and reads from it to do the input

--- Desktop/src/engine/Processing/algorithms/mgRecogniser.java
+++ Desktop/src/engine/Processing/algorithms/mgRecogniser.java
@@ -7,6 +7,7 @@ package engine.Processing.algorithms;
 
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 
 import engine.Processing.Processor;
 import engine.Processing.Recogniser;
@@ -16,29 +17,57 @@ import engine.util.*;
 import com.musicg.wave.*;
 import com.musicg.fingerprint.*;
 
-public class mgRecogniser implements Recogniser {
+public class mgRecogniser extends Recogniser {
 
 	private Counter counter;
 	Wave model,frag;
 	FingerprintSimilarity s;
+	Data toprocess;
+	
 	
 	public synchronized void setModel(String name){
 		model = new Wave(name);
 	}
 	
 	public mgRecogniser(Counter c){
+		super(c);
 		System.out.println("MusicG recogniser");
 		counter = c;
+		this.toprocess = new Data();
 		//c.enableIncremental();
 	}
 	
 	@Override
 	public void process(Data data) {
-		WaveHeader hdr = new WaveHeader();
-		hdr.setSampleRate(44100);
-		System.out.println(hdr.toString()+" is valid:"+hdr.isValid());
-		ByteArrayInputStream bais = new ByteArrayInputStream(data.getRaw());
-		frag = new Wave(bais);
+		this.toprocess.extend(data);
+		
+		if(this.toprocess.getLength() < this.model.getNormalizedAmplitudes().length)
+		{
+			return;
+		}
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		WavFile copy;
+		try
+		{
+			copy = WavFile.newWavFile(new File("./mgrecprocess.wav"), 1, this.toprocess.getLength(), 16, 44100);
+		} catch(Exception e)
+		{
+			e.printStackTrace();
+			return;
+		}
+		System.out.println("Stopped audio in");
+		System.out.println("Copying the file to the new file");
+		try
+		{
+			copy.writeFrames(this.toprocess.get(), this.toprocess.getLength());
+			copy.close();
+		} catch(Exception e)
+		{
+			e.printStackTrace();
+			return;
+		}
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		frag = new Wave("./mgrecprocess.wav");
 		s = model.getFingerprintSimilarity(frag);
 		System.out.println("Similarity:"+s.getSimilarity());
 		counter.increment(s.getSimilarity());
