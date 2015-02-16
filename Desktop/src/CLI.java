e60c50e424e9d2245e8ebe5329eea68d22bed414
unknown <2165422a@student.gla.ac.uk>
2015-02-16 16:43:26 +0000

    Added the copy file testing for wavfiles

--- Desktop/src/CLI.java
+++ Desktop/src/CLI.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.util.Scanner;
 
 import engine.Processing.*;
@@ -8,6 +9,8 @@ import engine.audio.AudioIn;
 import engine.audio.FileIn;
 import engine.util.Counter;
 import engine.util.Data;
+import engine.audio.WavFile;
+import engine.audio.WavFileException;
 
 public class CLI {
 
@@ -67,6 +70,46 @@ public class CLI {
 				rrr.process(d);
 				long endTime = System.currentTimeMillis();
 				System.out.println("Done:"+c.getCount()+" time:"+(double)(endTime-startTime)/1000);
+			}
+			else if(s.equals("copyFile")){
+				System.out.println("Initialising audio...");
+				AudioIn a = new FileIn("./tests/samples/clap_7_0.wav");
+				System.out.println("Starting audio_in");
+				a.blockingStart();
+				System.out.println("AudioIn finished now collecting data...");
+				Data d = new Data();
+				Data nxt;
+				while((nxt = a.getNext()) != null )
+				d.extend(nxt);
+				WavFile copy;
+				try
+				{
+					copy = WavFile.newWavFile(new File("./tests/results/copy.wav"), 1, d.getLength(), 16, 44100);
+				} catch(Exception e)
+				{
+					e.printStackTrace();
+					return;
+				}
+				a.stop();
+				System.out.println("Stopped audio in");
+				System.out.println("Copying the file to the new file");
+				try
+				{
+					copy.writeFrames(d.get(), d.getLength());
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+				System.out.println("Copied the file!");
+				try
+				{
+					copy.close();
+				} catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+				System.out.println("Done!");
 			}
 			else
 				System.out.println("Unknown command '"+s+"'");
