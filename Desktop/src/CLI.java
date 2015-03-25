79c04f2bf6075942fa88384724344ff7e3e3fc05
iFlex <milorad.liviu.felix@gmail.com>
2015-03-25 18:11:26 +0000

    Stabilised CLI

--- Desktop/src/CLI.java
+++ Desktop/src/CLI.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 import engine.Processing.*;
@@ -25,110 +26,126 @@ public class CLI {
 		processor.setInput("./tests/samples/clap_7_0.wav");
 		Scanner sc = new Scanner(System.in);
 		while(true){
-			String s = sc.nextLine();
-			
-			if(s.equals("blr"))
-				processor.blockingRun();
-			if(s.equals("changeAlgorithm") || s.equals("ca")){
-				String algname = sc.nextLine();
-				Recogniser r = null;
-				if(algname.equals("naive"))
-					r = new NaiveRecogniserMk3(count); 
-				if(algname.equals("rawridge"))
-					r = new RawRidgeRecogniser(count);
-				if(algname.equals("fastridge"))
-					r = new FastRidgeRecogniser(count);
-				if(algname.equals("fftridge"))
-					r = new FFTFastRidgeR(count);
-				//////////////////////////////////
-				processor = new Processor(count,r);
-				processor.setModel("./tests/models/clap.wav");
-				processor.setInput("./tests/samples/clap_7_0.wav");
-			}
-			if(s.equals("start") || s.equals("S"))
-				processor.start();
-			else if(s.equals("stop") || s.equals("s"))
-			{
-				processor.stop();
-				System.out.println("Count:"+count.getCount());
-			}
-			else if(s.equals("reset") || s.equals("r"))
-				count.reset();
-			
-			else if(s.equals("exit"))
-				break;
-			
-			else if(s.equals("setInput") || s.equals("si"))
-				processor.setInput(sc.nextLine());
-			
-			else if(s.equals("setModel") || s.equals("sm"))
-				processor.setModel(sc.nextLine());
-			
-			else if(s.equals("testFromRam")){
-				System.out.println("Initialising audio...");
-				AudioIn a = new FileIn("./tests/samples/clap_7_0.wav");
-				System.out.println("Starting audio_in");
-				a.blockingStart();
-				System.out.println("AudioIn finished now collecting data...");
-				Data d = new Data();
-				Data nxt;
-				while((nxt = a.getNext()) != null )
-				d.extend(nxt);
-				a.stop();
-				System.out.println("Stopped audio in");
-				Counter c = new Counter();
-				System.out.println("Starting recogniser");
-				RawRidgeRecogniser rrr = new RawRidgeRecogniser(c);
-				rrr.setModel("./tests/models/clap.wav");
-				System.out.println("Starting recogniser's processing");
-				long startTime = System.currentTimeMillis();
-				rrr.process(d);
-				long endTime = System.currentTimeMillis();
-				System.out.println("Done:"+c.getCount()+" time:"+(double)(endTime-startTime)/1000);
-			}
-			else if(s.equals("copyFile")){
-				System.out.println("Initialising audio...");
-				AudioIn a = new FileIn("./tests/samples/clap_7_0.wav");
-				System.out.println("Starting audio_in");
-				a.blockingStart();
-				System.out.println("AudioIn finished now collecting data...");
-				Data d = new Data();
-				Data nxt;
-				while((nxt = a.getNext()) != null )
-				d.extend(nxt);
-				WavFile copy;
-				try
-				{
-					copy = WavFile.newWavFile(new File("./tests/results/copy.wav"), 1, d.getLength(), 16, 44100);
-				} catch(Exception e)
-				{
-					e.printStackTrace();
-					return;
+			System.out.print("> ");
+			String s = sc.nextLine();	
+			try {
+				if(s.equals("blr"))
+					processor.blockingRun();
+				else if(s.equals("changeAlgorithm") || s.equals("ca")){
+					String algname = sc.nextLine();
+					Recogniser r = null;
+					if(algname.equals("naive"))
+						r = new NaiveRecogniserMk3(count); 
+					if(algname.equals("rawridge"))
+						r = new RawRidgeRecogniser(count);
+					if(algname.equals("fastridge"))
+						r = new FastRidgeRecogniser(count);
+					if(algname.equals("fftridge"))
+						r = new FFTFastRidgeR(count);
+					//////////////////////////////////
+					processor = new Processor(count,r);
+					processor.setModel("./tests/models/clap.wav");
+					processor.setInput("./tests/samples/clap_7_0.wav");
 				}
-				a.stop();
-				System.out.println("Stopped audio in");
-				System.out.println("Copying the file to the new file");
-				try
+				else if(s.equals("start") || s.equals("S"))
+					processor.start();
+				else if(s.equals("stop") || s.equals("s"))
 				{
-					copy.writeFrames(d.get(), d.getLength());
+					processor.stop();
+					System.out.println("Count:"+count.getCount());
 				}
-				catch(Exception e)
-				{
-					e.printStackTrace();
+				else if(s.equals("reset") || s.equals("r"))
+					count.reset();
+				
+				else if(s.equals("exit"))
+					break;
+				else if(s.equals("wavHeader")){
+			         WavFile wavFile = null;
+					try {
+						wavFile = WavFile.openWavFile(new File(sc.nextLine()));
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (WavFileException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+			         wavFile.display();
 				}
-				System.out.println("Copied the file!");
-				try
-				{
-					copy.close();
-				} catch(Exception e)
-				{
-					e.printStackTrace();
+				else if(s.equals("setInput") || s.equals("si"))
+					processor.setInput(sc.nextLine());
+				
+				else if(s.equals("setModel") || s.equals("sm"))
+					processor.setModel(sc.nextLine());
+					
+				else if(s.equals("testFromRam")){
+					System.out.println("Initialising audio...");
+					AudioIn a = new FileIn("./tests/samples/clap_7_0.wav");
+					System.out.println("Starting audio_in");
+					a.blockingStart();
+					System.out.println("AudioIn finished now collecting data...");
+					Data d = new Data();
+					Data nxt;
+					while((nxt = a.getNext()) != null )
+					d.extend(nxt);
+					a.stop();
+					System.out.println("Stopped audio in");
+					Counter c = new Counter();
+					System.out.println("Starting recogniser");
+					RawRidgeRecogniser rrr = new RawRidgeRecogniser(c);
+					rrr.setModel("./tests/models/clap.wav");
+					System.out.println("Starting recogniser's processing");
+					long startTime = System.currentTimeMillis();
+					rrr.process(d);
+					long endTime = System.currentTimeMillis();
+					System.out.println("Done:"+c.getCount()+" time:"+(double)(endTime-startTime)/1000);
 				}
-				System.out.println("Done!");
+				else if(s.equals("copyFile")){
+					System.out.println("Initialising audio...");
+					AudioIn a = new FileIn("./tests/samples/clap_7_0.wav");
+					System.out.println("Starting audio_in");
+					a.blockingStart();
+					System.out.println("AudioIn finished now collecting data...");
+					Data d = new Data();
+					Data nxt;
+					while((nxt = a.getNext()) != null )
+					d.extend(nxt);
+					WavFile copy;
+					try
+					{
+						copy = WavFile.newWavFile(new File("./tests/results/copy.wav"), 1, d.getLength(), 16, 44100);
+					} catch(Exception e)
+					{
+						e.printStackTrace();
+						return;
+					}
+					a.stop();
+					System.out.println("Stopped audio in");
+					System.out.println("Copying the file to the new file");
+					try
+					{
+						copy.writeFrames(d.get(), d.getLength());
+					}
+					catch(Exception e)
+					{
+						e.printStackTrace();
+					}
+					System.out.println("Copied the file!");
+					try
+					{
+						copy.close();
+					} catch(Exception e)
+					{
+						e.printStackTrace();
+					}
+					System.out.println("Done!");
+				}
+				else
+					System.out.println("Unknown command '"+s+"'");
 			}
-			else
-				System.out.println("Unknown command '"+s+"'");
-			
+			catch( Exception e ){
+				System.out.println("#ERROR:"+e.toString());
+			}
 		}
 		System.out.println("Bye!");
 	}
